d5a0b1c9a7cf78b580bd5efe14e11e61f45d5d1e
GyeongMin Kang <k12e5989@gmail.com>
2021-01-05 12:32:32 +0900

    kotlin-android-training-repository

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.devbyteviewer.network
 
+import com.example.android.devbyteviewer.database.DatabaseVideo
 import com.example.android.devbyteviewer.domain.DevByteVideo
 import com.squareup.moshi.JsonClass
 
@@ -51,12 +52,23 @@ data class NetworkVideo(
         val thumbnail: String,
         val closedCaptions: String?)
 
-/**
- * Convert Network results to database objects
- */
 fun NetworkVideoContainer.asDomainModel(): List<DevByteVideo> {
     return videos.map {
         DevByteVideo(
+                title = it.title,
+                description = it.description,
+                url = it.url,
+                updated = it.updated,
+                thumbnail = it.thumbnail)
+    }
+}
+
+/**
+ * Convert Network results to database objects
+ */
+fun NetworkVideoContainer.asDatabaseModel(): List<DatabaseVideo> {
+    return videos.map {
+        DatabaseVideo(
                 title = it.title,
                 description = it.description,
                 url = it.url,
